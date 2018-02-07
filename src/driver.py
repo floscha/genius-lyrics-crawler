503c2a062030ed6f517f8c7ce26d34a8ab89070b
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-02-07 15:25:29 +0100

    Add option to crawl songs from a single artist to driver CLI

--- src/driver.py
+++ src/driver.py
@@ -2,18 +2,21 @@ import argparse
 
 from genius_crawler import scrape_song
 from genius_crawler import scrape_songs
+from genius_crawler import scrape_songs_of_artist
 
 
 def cli():
     # Read and parse command line arguments.
     parser = argparse.ArgumentParser()
-    parser.add_argument('target', choices=['all', 'popular', 'song'],
+    target_choices = ['all', 'popular', 'artist', 'song']
+    parser.add_argument('target', choices=target_choices,
                         help='defines wether all song, only popular ' +
-                        'songs, or a single song should be scraped')
+                             'songs, song from a single artist, or only a ' +
+                             'single song should be scraped')
     # Take letters of type `list` so that 'abc' becomes ['a', 'b', 'c'].
     parser.add_argument('-l', '--letters', type=list,
                         help='only scrape songs from artists beginning ' +
-                        'with one of the specified letters')
+                             'with one of the specified letters')
     parser.add_argument('-apl', '--artists_per_letter', type=int,
                         help='number of artist per letter to scrape')
     parser.add_argument('-ppa', '--pages_per_artist', type=int,
@@ -33,9 +36,19 @@ def cli():
                      artists_per_letter=args.artists_per_letter,
                      pages_per_artist=args.pages_per_artist,
                      songs_per_page=args.songs_per_page)
+    elif args.target == 'artist':
+        if args.artist:
+            scrape_songs_of_artist.delay(
+                artist_name=args.artist,
+                pages_per_artist=args.pages_per_artist,
+                songs_per_page=args.songs_per_page
+            )
+        else:
+            raise ValueError("The --artist parameter has to be set when " +
+                             "using 'artist' as target")
     elif args.target == 'song':
         if args.artist and args.title:
-            scrape_song.delay(args.artist, args.title)
+            scrape_song.delay(artist=args.artist, title=args.title)
         else:
             raise ValueError("Both --artist and --title parameters have to " +
                              "be set when using 'song' as target")
