1b4a5008b96e810e2eaf1449bf70514b45675988
Florian Schäfer <florian.joh.schaefer@gmail.com>
2018-05-26 18:05:28 +0200

    Raise exception instead of using assert

--- src/worker/celery_worker.py
+++ src/worker/celery_worker.py
@@ -6,7 +6,10 @@ from celery import Celery
 # Setup Celery with RabbitMQ as the broker.
 broker_user = os.getenv('RABBITMQ_USER')
 broker_pass = os.getenv('RABBITMQ_PASS')
-assert broker_pass and broker_pass, "Both 'RABBITMQ_USER' and " + \
-                                    "'RABBITMQ_PASS' have to be set in ENV"
+
+if not broker_pass or not broker_pass:
+    raise ValueError("Both 'RABBITMQ_USER' and 'RABBITMQ_PASS' have to be set"
+                     + " in ENV")
+
 worker = Celery('genius_lyrics_crawler',
                 broker='amqp://gavin:hooli@rabbitmq:5672')
